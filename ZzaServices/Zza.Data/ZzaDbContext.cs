0db4932ca79bfdc945cc1be6d3d34355d3da2a6f
YGORJS <ygorjs26@gmail.com>
2016-08-03 16:06:03 -0300

    Commit inicial

---
+++ ZzaServices/Zza.Data/ZzaDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Entities;
+
+namespace Zza.Data
+{
+    public class ZzaDbContext:DbContext
+    {
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderItem> OrdemItems { get; set; }
+        public DbSet<OrderStatus> OrderStatuses{ get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
